cf37aba5a3341c8d0bcd75a36e021ca5a8bc62d0
holyoke <jasonebueng@gmail.com>
2017-03-06 16:16:36 -0800

    adds Rating model spec

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,5 @@
 class Rating < ApplicationRecord
+  validates_presence_of :score
+  validates_numericality_of :score, only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100
+  belongs_to :film
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:score) }
+  it { should validate_numericality_of(:score).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(100).only_integer }
+  it { should belong_to(:film) }
 end
