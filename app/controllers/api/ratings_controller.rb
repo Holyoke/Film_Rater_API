cdb4d7c300bb58de636a8103ceb5fe4748c79f80
holyoke <jasonebueng@gmail.com>
2017-03-06 16:40:55 -0800

    introduces api ratings controller

---
+++ app/controllers/api/ratings_controller.rb
@@ -0,0 +1,22 @@
+class Api::RatingsController < ApplicationController
+  before_action :set_film
+
+  def create
+    rating = @film.ratings.new(rating_params)
+
+    if rating.save
+      render json: rating
+    else
+      render json: rating.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_film
+      @film = params[:film_id]
+    end
+
+    def rating_params
+      params.require(:rating).permit(:score)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :films, only: [:index, :show]
+    resources :ratings, only: [:create, :update, :destroy]
   end
 end
